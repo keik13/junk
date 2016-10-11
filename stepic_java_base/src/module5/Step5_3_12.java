154d8fa53142cb098db4ab5c110e8042a3567b63
KobAA <a.koba@bssys.com>
2016-10-11 18:51:23 +0300

    step5_3_12

---
+++ stepic_java_base/src/module5/Step5_3_12.java
@@ -0,0 +1,24 @@
+package module5;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+/**
+ * Реализуйте метод, который зачитает данные из InputStream и преобразует их в строку, используя заданную кодировку.
+ * <p>
+ * Пример
+ * <p>
+ * InputStream последовательно возвращает четыре байта: 48 49 50 51.
+ * <p>
+ * Метод, вызванный для такого InputStream и кодировки ASCII, должен вернуть строку "0123".
+ *
+ * @author Andrey Koba)
+ */
+
+public class Step5_3_12 {
+    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
+        // your implementation here
+        return null;
+    }
+}
