ca643a33983013ab8ea15235fabe16ad62ad61d9
KobAA <a.koba@bssys.com>
2017-08-16 19:28:18 +0300

    add E08 binary

--- exercises_thinking_in_java/src/main/java/ch3/operators/E08_HexOctalNotations.java
+++ exercises_thinking_in_java/src/main/java/ch3/operators/E08_HexOctalNotations.java
@@ -11,7 +11,9 @@ public class E08_HexOctalNotations {
     public static void main(String[] args) {
         long hex = 0X14fffffffL;
         long octal = 0447;
+        long bin = 0b01101010101001010101010101010101;
         System.out.println("hex - " + Long.toBinaryString(hex));
         System.out.println("octal - " + Long.toBinaryString(octal));
+        System.out.println("binary - " + bin);
     }
 }
