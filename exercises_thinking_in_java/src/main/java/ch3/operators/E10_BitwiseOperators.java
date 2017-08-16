f1184f8fc0c2c838112782ab7eead8c4a723e26a
KobAA <a.koba@bssys.com>
2017-08-16 20:24:51 +0300

    add E10

---
+++ exercises_thinking_in_java/src/main/java/ch3/operators/E10_BitwiseOperators.java
@@ -0,0 +1,30 @@
+package ch3.operators;
+
+/**
+ * Write a program with two constant values, one with alternating binary
+ * ones and zeroes, with a zero in the least-significant digit, and the second, also alternating,
+ * with a one in the least-significant digit (hint: It’s easiest to use hexadecimal constants for
+ * this). Take these two values and combine them in all possible ways using the bitwise
+ * operators, and display the results using Integer.toBinaryString( ).
+ *
+ * @author Andrey Koba)
+ */
+
+public class E10_BitwiseOperators {
+    public static void main(String[] args) {
+        int a = 0b1010101010;
+        int b = 0b0101010101;
+
+        System.out.println("a - " + Integer.toBinaryString(a));
+        System.out.println("b - " + Integer.toBinaryString(b));
+        System.out.println("a >> b - " + Integer.toBinaryString(a >> b));
+        System.out.println("a << b - " + Integer.toBinaryString(a << b));
+        System.out.println("a >>> b - " + Integer.toBinaryString(a >>> b));
+        System.out.println("a & b - " + Integer.toBinaryString(a & b));
+        System.out.println("a | b - " + Integer.toBinaryString(a | b));
+        System.out.println("a ^ b - " + Integer.toBinaryString(a ^ b));
+        System.out.println("~a - " + Integer.toBinaryString(~a));
+        System.out.println("~b - " + Integer.toBinaryString(~b));
+
+    }
+}
