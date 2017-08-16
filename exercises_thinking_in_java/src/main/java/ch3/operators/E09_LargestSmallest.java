8eabf7fd323a79de28db86390ba88e263de5f169
KobAA <a.koba@bssys.com>
2017-08-16 19:48:44 +0300

    add E09

---
+++ exercises_thinking_in_java/src/main/java/ch3/operators/E09_LargestSmallest.java
@@ -0,0 +1,17 @@
+package ch3.operators;
+
+/**
+ * Display the largest and smallest numbers for both float and double
+ * exponential notation.
+ *
+ * @author Andrey Koba)
+ */
+
+public class E09_LargestSmallest {
+    public static void main(String[] args) {
+        System.out.println("float min - " + Float.MIN_VALUE);
+        System.out.println("float min - " + Float.MAX_VALUE);
+        System.out.println("double min - " + Double.MAX_VALUE);
+        System.out.println("double min - " + Double.MAX_VALUE);
+    }
+}
